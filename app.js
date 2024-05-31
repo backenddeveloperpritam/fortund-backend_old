18da77349bdbc81cd23f568547c52352d818ea5c
pritam <backenddeveloperpritam@gmail.com>
2024-05-31 15:49:15 +0530

    app

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
 import express from "express";
 import db from "./config/db.js";
 
-// import customerRoutes from "./routes/customerRoutes.js";
-// import kundliRoutes from "./routes/kundliRoutes.js";
-// import adminRoutes from "./routes/adminRoutes.js";
-import appRoutes from "./routes/application/application.routes.js"
-import astroAppRoutes from "./routes/application/astrologer.routes.js"
+import appRoutes from "./routes/application/application.routes.js";
+import astroAppRoutes from "./routes/application/astrologer.routes.js";
 import astrologerRequestRoute from "./routes/application/astrologerRequest.routes.js";
 
-// import astrologerRoutes from "./routes/astrologerRoutes.js";
-// import webRoutes from './routes/webRoutes.js';
-// import blogCategoryRoute from "./routes/admin/blogCagegory.route.js";
+import blogCategoryRoute from "./routes/admin/blogcagegory.route.js";
 import astrologerRoute from "./routes/admin/astrologer.route.js";
 import SkillRoute from "./routes/admin/skill.route.js";
 import SubSkillRoute from "./routes/admin/subSkill.route.js";
 import ExpertiesRoute from "./routes/admin/expertise.route.js";
-
 
 import cors from "cors";
 import { createServer } from "http";
@@ -36,33 +30,23 @@ const io = new Server(httpServer, {
 
 const PORT = process.env.PORT || 4000;
 
+console.log(`Ports: ${PORT}`);
+
 app.use(express.static(__dirname));
 app.use(express.json());
 app.use(cors('*'));
 db();
-// // Define the API routes
-app.use("/api/app", appRoutes)
-app.use("/api/app", astroAppRoutes)
-app.use("/api/app", astrologerRequestRoute)
-// app.use("/api/customers", customerRoutes);
-// app.use("/api/admin", adminRoutes);
-// app.use("/api/astrologer", astrologerRoutes);
-// app.use('/api/kundli', kundliRoutes);
-// app.use('/api/web', webRoutes);
+
+// Define the API routes
+app.use("/api/app", appRoutes);
+app.use("/api/app", astroAppRoutes);
+app.use("/api/app", astrologerRequestRoute);
 
 app.use("/api/admin", astrologerRoute);
-// app.use('/api/admin', blogCategoryRoute);
+app.use('/api/admin', blogCategoryRoute);
 app.use('/api/admin', SkillRoute);
 app.use('/api/admin', SubSkillRoute);
 app.use('/api/admin', ExpertiesRoute);
-
-
-// app.use("/api/kundli", kundliRoutes) 
-// app.use('/notification', notificationRoutes);
-
-// app.use('/images', express.static('uploadImage'));
-
-// initializeSocketIO(io)
 
 httpServer.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
